30728fc4a721003ea47addf08853359701912339
Haris <haris@testing.gr>
2017-12-11 16:14:31 +0200

    comments etc

--- PumpAnalysis/frmPumpAnalysis.cs
+++ PumpAnalysis/frmPumpAnalysis.cs
@@ -34,6 +34,13 @@ namespace PumpAnalysis
 
             List<ImpData> DataToMigrate = dbu.JsonToObjectList(read_data);
 
+            //?? show to grid
+            
+            //?? 2nd import of the same file ??
+
+            //import txt file as blob into sqlite
+            //import the whole file into sql DB table - imported group
+
             bool insSuccess = dbu.ObjectList_To_SQLServerReceiptDataLines(DataToMigrate, read_data);
             if (insSuccess == false)
             {
@@ -43,6 +50,8 @@ namespace PumpAnalysis
             {
                 MessageBox.Show("Η διαδικασία ολοκληρώθηκε επιτυχώς!");
             }
+
+            
 
         }
     }
