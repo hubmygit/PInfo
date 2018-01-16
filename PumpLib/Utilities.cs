97d3cb55a5ad0688c0af66142ceb7c748e1b8da3
Haris <haris@testing.gr>
2018-01-16 15:48:45 +0200

    MapFormParams & MapFormGeoData classes

--- PumpLib/Utilities.cs
+++ PumpLib/Utilities.cs
@@ -1650,6 +1650,22 @@ namespace PumpLib
         }
     }
 
+    public class MapFormParams
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public int radius { get; set; }
+        public string connectionString { get; set; }
+        public string apiKey { get; set; }
+        public bool existsInternetConnection { get; set; }
+    }
+
+    public class MapFormGeoData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
 
     //--> Google Geocoding classes
     public class GeoCoding
