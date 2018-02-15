ce09aed398f346327894a0475783215632c36e2b
leokapaslg <leokapaslg@gmail.com>
2018-02-15 16:00:34 +0200

    //

--- MapForm/Program.cs
+++ MapForm/Program.cs
@@ -16,6 +16,7 @@ namespace MapForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SearchPlace());
+            //
         }
     }
 }
