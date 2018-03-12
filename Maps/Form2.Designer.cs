dd34ff9d32220cc50f756f58b219b513d25b38c2
Haris <haris@testing.gr>
2018-03-12 16:08:51 +0200

    max radius from 5000 to 10000

--- Maps/Form2.Designer.cs
+++ Maps/Form2.Designer.cs
@@ -166,7 +166,7 @@
             0});
             this.numericUpDown1.Location = new System.Drawing.Point(84, 5);
             this.numericUpDown1.Maximum = new decimal(new int[] {
-            5000,
+            10000,
             0,
             0,
             0});
